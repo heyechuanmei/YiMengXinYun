6ce42859880d78726a3009367b2219745291470f
heyechuanmei <42758394+heyechuanmei@users.noreply.github.com>
2021-02-01 10:57:21 +0800

    Create jiaqid.js

---
+++ static/life/jiaqid.js
@@ -0,0 +1,1 @@
+console.log('\n' + ' %c Made by love from 假期哒 ' + ' %c https://www.jiaqid.com ' + '\n' + '\n', 'color: #fff; background: #fd79a8; padding:5px 0;', 'background: #FFF; padding:5px 0;');
